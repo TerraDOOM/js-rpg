a65146ae81d5508811cea23cde04463e071692ac
koupip <koupip@gmail.com>
2019-07-15 17:24:54 +0200

    graphic engine test bullshit

--- test/mapGeneration.js
+++ test/mapGeneration.js
@@ -1,8 +1,16 @@
+class tileSet{
+    constructor(name,color,move){
+        this.name = name;
+        this.color = color;
+        this.move = move;
+    }
+}
 function generateMap(){
     let map = [];
     const zoneType = ['forest','dungeon','cave','town'];
+    let basicTyle = new tileSet("grass","green",true);
     for (let i = 0;i<10;i++){
-        map[i]= ["grass","grass","grass","grass","grass","grass","grass","grass","grass","grass"];
+        map[i]= [basicTyle,basicTyle,basicTyle,basicTyle,basicTyle,basicTyle,basicTyle,basicTyle,basicTyle,basicTyle,];
     }
     for(let i=0;i<5;i++){
         let first = rng(0,9);
@@ -15,29 +23,37 @@ function generateMap(){
 }
 function forestZone(){
     let map = [];
+    let basicTyle = new tileSet("grass","green",true);
+    let exitTyle = new tileSet("exit","grey",true);
     for (let i = 0;i<5;i++){
-        map[i]= ["grass","grass","grass","grass","grass"];
+        map[i]= [exitTyle,basicTyle,basicTyle,basicTyle,basicTyle];
     }
     return map;
 }
 function dungeonZone(){
     let map = [];
+    let basicTyle = new tileSet("rockFloor","brown",true);
+    let exitTyle = new tileSet("exit","grey",true);
     for (let i = 0;i<5;i++){
-        map[i]= ["rockFloor","rockFloor","rockFloor","rockFloor","rockFloor"];
+        map[i]= [exitTyle,basicTyle,basicTyle,basicTyle,basicTyle];
     }
     return map;
 }
 function caveZone(){
     let map = [];
+    let basicTyle = new tileSet("rockFloor","brown",true);
+    let exitTyle = new tileSet("exit","grey",true);
     for (let i = 0;i<5;i++){
-        map[i]= ["rockFloor","rockFloor","rockFloor","rockFloor","rockFloor"];
+        map[i]= [exitTyle,basicTyle,basicTyle,basicTyle,basicTyle];
     }
     return map;
 }
 function townZone(){
     let map = [];
+    let basicTyle = new tileSet("road","brown",true);
+    let exitTyle = new tileSet("exit","grey",true);
     for (let i = 0;i<5;i++){
-        map[i]= ["road","road","road","road","road"];
+        map[i]= [exitTyle,basicTyle,basicTyle,basicTyle,basicTyle];
     }
     return map;
 }
